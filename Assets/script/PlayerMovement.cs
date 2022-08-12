0d29aa39d2d132dce6f33296b69a5a86efeba7f5
B00125508 <75093481+B00125508@users.noreply.github.com>
2022-08-12 15:13:22 +0100

    All of the files

---
+++ Assets/script/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    [SerializeField] private float speed = 5.0f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontal, 0, vertical) * (speed * Time.deltaTime));
+    
+    } 
+}
